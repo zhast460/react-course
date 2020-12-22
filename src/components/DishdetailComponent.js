bd270c3fab996eaabb181f02554cce40a9e2000a
Scott <cmoshadow@gmail.com>
2020-12-22 21:29:12 +0000

    Header and Footer

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -23,19 +23,21 @@ const DishDetail = (props) => {
     const dish = props.dish
     if (dish) {
         return (
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg top src={dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-                <div className="col-12 col-md-5 m-1">
-                    <h4>Comments</h4>
-                    <RenderComments comments={dish.comments} />
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <Card>
+                            <CardImg top src={dish.image} alt={dish.name} />
+                            <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <h4>Comments</h4>
+                        <RenderComments comments={dish.comments} />
+                    </div>
                 </div>
             </div>
         );
